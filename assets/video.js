f0edfef269fb3a452f002cd253ad0e03ece26b3f
Sher Vera <vera8german@gmail.com>
2022-02-03 18:20:37 -0400

    merge

---
+++ assets/video.js
@@ -0,0 +1,145 @@
+window.addEventListener("load", () => {
+  const login = document.getElementById("login");
+  const loginForm = document.getElementById("login-form");
+  const identityField = document.getElementById("identity");
+  const roomNameField = document.getElementById("roomName");
+  const chat = document.getElementById("chat");
+  const participants = document.getElementById("participants");
+  const usernameSpan = document.getElementById("username");
+  const roomSpan = document.getElementById("room");
+
+  const videoButton = document.getElementById("play-video");
+  const videoElement = document.getElementById("big-buck-bunny");
+
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const identity = identityField.value;
+    const roomName = roomNameField.value;
+    login.setAttribute("hidden", "true");
+    console.log(roomName);
+    // Fetch the access token
+    fetch("https://bz905h9z76.execute-api.us-east-1.amazonaws.com/dev/video/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ identity: identity, room: roomName }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        const { token, room, identity } = response
+        console.log(token, room, identity);
+        usernameSpan.textContent = identity;
+        roomSpan.textContent = room;
+        chat.removeAttribute("hidden");
+        startVideoChat(room, token);
+      });
+  });
+
+  function startVideoChat(room, token) {
+    console.log(room, token);
+    // Start video chat and listen to participant connected events
+    Twilio.Video.connect(token, {
+      room: room,
+      audio: true,
+      video: true,
+    }).then((room) => {
+      console.log('room', room);
+      // Once we're connected to the room, add the local participant to the page
+      participantConnected(room.localParticipant);
+      // Add any existing participants to the page.
+      room.participants.forEach(participantConnected);
+      // Listen for other participants to join and add them to the page when they
+      // do.
+      room.on("participantConnected", participantConnected);
+      // Listen for participants to leave the room and remove them from the page
+      room.on("participantDisconnected", participantDisconnected);
+      // Eject the participant from the room if they reload or leave the page
+      window.addEventListener("beforeunload", tidyUp(room));
+      window.addEventListener("pagehide", tidyUp(room));
+
+      // Show the video and button
+      videoButton.removeAttribute("hidden");
+      videoElement.removeAttribute("hidden");
+      let playing = false;
+      let tracks;
+      videoButton.addEventListener("click", () => {
+        if (!playing) {
+          const mediaStream = videoElement.captureStream();
+          videoElement.play();
+          tracks = mediaStream.getTracks();
+          console.log(tracks);
+          room.localParticipant.publishTracks(tracks);
+          playing = true;
+          videoButton.innerText = "Stop";
+        } else {
+          playing = false;
+          videoElement.pause();
+          room.localParticipant.unpublishTracks(tracks);
+          videoButton.innerText = "Play this video to the room";
+        }
+      });
+    });
+  }
+
+  function participantConnected(participant) {
+    // Create new <div> for participant and add it to the page
+    const el = document.createElement("div");
+    el.setAttribute("id", participant.identity);
+    participants.appendChild(el);
+    // Find all the participant's existing tracks and publish them to our page
+    participant.tracks.forEach((trackPublication) => {
+      trackPublished(participant)(trackPublication);
+    });
+    // Listen for the participant publishing new tracks
+    participant.on("trackPublished", trackPublished(participant));
+  }
+
+  function trackPublished(participant) {
+    return function (trackPublication) {
+      console.log(trackPublication);
+      // Get the participant's <div> we created earlier
+      const el = document.getElementById(participant.identity);
+      // Find out if the track has been subscribed to and add it to the page or
+      // listen for the subscription, then add it to the page.
+
+      // First create a function that adds the track to the page
+      const trackSubscribed = (track) => {
+        // track.attach() creates the media elements <video> and <audio> to
+        // to display the track on the page.
+        el.appendChild(track.attach());
+      };
+      // If the track is already subscribed, add it immediately to the page
+      if (trackPublication.track) {
+        trackSubscribed(trackPublication.track);
+      }
+      // Otherwise listen for the track to be subscribed to, then add it to the
+      // page
+      trackPublication.on("subscribed", trackSubscribed);
+    };
+  }
+
+  function participantDisconnected(participant) {
+    participant.removeAllListeners();
+    const el = document.getElementById(participant.identity);
+    el.remove();
+  }
+
+  function trackUnpublished(trackPublication) {
+    trackPublication.track.detach().forEach(function (mediaElement) {
+      mediaElement.remove();
+    });
+  }
+
+  function tidyUp(room) {
+    return function (event) {
+      if (event.persisted) {
+        return;
+      }
+      if (room) {
+        room.disconnect();
+        room = null;
+      }
+    };
+  }
+});
